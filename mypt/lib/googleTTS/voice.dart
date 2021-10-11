72d15c0e83d76b4019a401ddabefd28f93d808dd
MuSeongPark <65115418+MuSeongPark@users.noreply.github.com>
2021-10-11 04:41:16 +0000

    google TTS fixed v1.2

--- mypt/lib/googleTTS/voice.dart
+++ mypt/lib/googleTTS/voice.dart
@@ -1,100 +1,115 @@
 
 import 'package:flutter_tts/flutter_tts.dart';
+import 'dart:async';
 
 class Voice{
   final FlutterTts tts = FlutterTts();
-  Voice(){
+  Voice() {
     tts.setLanguage('ko');
     tts.setSpeechRate(0.5);
     tts.setVolume(1);
-    tts.awaitSpeakCompletion(true);
   }
 
-  void sayStart(){
+  void sayStart() async {
     tts.speak('시작합니다.');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
   }
 
-  void countingVoice(int count){
+  void countingVoice(int count) async {
     if (count ~/ 5 == 0){
       String countStr = count.toString();
       tts.speak(countStr + '개');
-      //tts.awaitSpeakCompletion(true);
+      await tts.awaitSpeakCompletion(true);
     }
   }
 
-  void sayStretchElbow(){
+  void sayStretchElbow() async {
     tts.speak('팔꿈치를 더 펴 주세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayBendElbow(){
+  void sayBendElbow() async {
     tts.speak('팔꿈치를 더 굽혀 주세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayStretchElbowAndShoulder(){
+  void sayStretchElbowAndShoulder() async {
     tts.speak('팔꿈치와 어깨를 더 펴주세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayHipUp(){
+  void sayHipUp() async {
     tts.speak('골반을 올려주세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayHipDown(){
+  void sayHipDown() async {
     tts.speak('골반을 내려주세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayHipKnee(){
+  void sayHipKnee() async {
     tts.speak('엉덩이과 무릎이 같이 내려가야합니다');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayKneeUp(){
+  void sayKneeUp() async {
     tts.speak('무릎을 올리세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayStretchKnee(){
+  void sayStretchKnee() async {
     tts.speak('무릎을 펴세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayBendKnee(){
+  void sayBendKnee() async {
     tts.speak('무릎을 굽히세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayKneeOut(){
+  void sayKneeOut() async {
     tts.speak('무릎과 발이 수직이 되게 하세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayUp(){
+  void sayUp() async {
     tts.speak('끝까지 올라가세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayElbowFixed(){
+  void sayElbowFixed() async {
     tts.speak('팔꿈치를 고정하세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayFast(){
+  void sayFast() async {
     tts.speak('조금 천천히 해주세요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayGood1(){
+  void sayGood1() async {
     tts.speak('좋아요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 
-  void sayGood2(){
+  void sayGood2() async {
     tts.speak('잘하고 있어요');
-    //tts.awaitSpeakCompletion(true);
+    await tts.awaitSpeakCompletion(true);
+
   }
 }
