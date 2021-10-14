17b958cc549f8022113a8a88bd79b4848bee2d8d
alsrnwlgp <alsrnwlgp@dgist.ac.kr>
2021-10-14 11:08:04 +0000

    add barchart to category list page

--- mypt/lib/screens/category_list_page.dart
+++ mypt/lib/screens/category_list_page.dart
@@ -7,7 +7,7 @@ import 'package:mypt/screens/main_page.dart';
 import 'package:mypt/components/record_grid.dart';
 import 'package:mypt/screens/workout_description_page.dart';
 import 'package:mypt/models/workout_result.dart';
-import 'package:mypt/screens/analysis/result_page.dart';
+import 'package:mypt/screens/analysis/bar_chart_page.dart';
 
 class CategoryListPage extends StatelessWidget {
   CategoryListPage({Key? key}) : super(key: key);
@@ -36,7 +36,7 @@ class CategoryListPage extends StatelessWidget {
             backgroundColor: colorList[index % colorList.length],
             //nextPage: WorkoutDescriptionPage(workoutName: muscleList.keys.toList()[index]),
             // 일단은 주석처리 해놓음. Chest, Legs, Abs 등 부위별로 분리할 시 아래 코드 응용하면 됌
-            nextPage: ResultPage(workoutResult: workoutResultList[0])
+            nextPage: BarChartPage()
           ),
         ),
       ),
