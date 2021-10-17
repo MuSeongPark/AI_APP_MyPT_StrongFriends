f77b5215840ee80ce6a1a1d180c56056fd08a779
alsrnwlgp <alsrnwlgp@dgist.ac.kr>
2021-10-17 06:46:05 +0000

    reset pre version

--- mypt/lib/screens/analysis/workout_result_page.dart
+++ mypt/lib/screens/analysis/workout_result_page.dart
@@ -5,6 +5,12 @@ import 'package:mypt/utils/build_no_title_appbar.dart';
 import 'package:mypt/models/workout_result.dart';
 import 'package:mypt/utils.dart';
 
+import 'package:flutter/material.dart';
+import 'package:mypt/components/customized_bar_chart.dart';
+import 'package:mypt/theme.dart';
+import 'package:mypt/utils/build_no_title_appbar.dart';
+import 'package:mypt/models/workout_result.dart';
+
 class WorkoutResultPage extends StatelessWidget {
   WorkoutResultPage({Key? key, required this.workoutResult}) : super(key: key);
   WorkoutResult workoutResult;
@@ -14,30 +20,20 @@ class WorkoutResultPage extends StatelessWidget {
     return Scaffold(
       appBar: buildNoTitleAppBar(),
       body: Container(
-        padding: const EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10.0),
-              child: CustomizedBarChart(
-                workoutResult: workoutResult,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                margin: const EdgeInsets.only(top: 15),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    color: kLightIvoryColor,
-                    child: Center(
-                      child: _buildFeedback(),
-                    ),
-                  ),
+            CustomizedBarChart(workoutResult: workoutResult,),
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 250,
+                  width: double.infinity,
+                  color: kLightIvoryColor,
+                  child: Center(child: _buildFeedback()),
                 ),
               ),
             ),
@@ -47,7 +43,7 @@ class WorkoutResultPage extends StatelessWidget {
     );
   }
 
-  Widget _buildFeedback() {
+    Widget _buildFeedback() {
     List<int> feedbackIdx = sortFeedback(workoutResult.feedbackCounts!);
     List<String> feedbackString;
     if (workoutResult.workoutName! == 'push_up'){
